a18ad3142ac85aca5e46f16b482131f77f4aefe3
Jeffrey Monzili <jeffrey.monzili@gmail.com>
2022-09-23 16:31:58 +0200

    Correction des warning

--- ohmyfood/src/assets/data/restaurantsList.js
+++ ohmyfood/src/assets/data/restaurantsList.js
@@ -1,34 +1,32 @@
-import { v4 as uuidv4 } from 'uuid';
-
 const restaurantList = [
   {
-    name: "La palette du goût",
-    city: "Ménilmontant",
+    name: 'La palette du goût',
+    city: 'Ménilmontant',
     id: 1,
-    img: "./imgs/la_palette_du_gout.jpg",
+    img: './imgs/la_palette_du_gout.jpg',
     isNew: true,
   },
   {
-    name: "La note enchantée",
-    city: "Charonne",
+    name: 'La note enchantée',
+    city: 'Charonne',
     id: 2,
-    img: "./imgs/la_note_enchantee.jpg",
+    img: './imgs/la_note_enchantee.jpg',
     isNew: true,
   },
   {
-    name: "À la française",
-    city: "Cité Rouge",
+    name: 'À la française',
+    city: 'Cité Rouge',
     id: 3,
-    img: "./imgs/a_la_francaise.jpg",
+    img: './imgs/a_la_francaise.jpg',
     isNew: false,
   },
   {
-    name: "Le délice des sens",
-    city: "Folie-Méricourt",
+    name: 'Le délice des sens',
+    city: 'Folie-Méricourt',
     id: 4,
-    img: "./imgs/le_delice_des_sens.jpg",
+    img: './imgs/le_delice_des_sens.jpg',
     isNew: false,
   },
 ];
 
-export default restaurantList
+export default restaurantList;
